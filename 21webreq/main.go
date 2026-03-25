ef251543cb9268282ec39c74e7d033d9bb4fcfd0
Brajeswar Ghosh <brajeswar.ghosh@mihup.com>
2026-03-25 17:56:31 +0530

    updated webreq

--- 21webreq/main.go
+++ 21webreq/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -11,7 +12,8 @@ func main() {
 	fmt.Println("Welcome to web verb video - LCO")
 
 	// PerformGetRequest()
-	PerformPostJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetRequest() {
@@ -53,6 +55,29 @@ func PerformPostJsonRequest() {
 
 	response, err := http.Post(myurl, "application/json", requestBody)
 
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//formdata
+
+	data := url.Values{}
+	data.Add("FirstName", "Brajeswar")
+	data.Add("LastName", "Ghosh")
+	data.Add("email", "brajeswar@go.dev")
+
+	response, err := http.PostForm(myurl, data)
 	if err != nil {
 		panic(err)
 	}
