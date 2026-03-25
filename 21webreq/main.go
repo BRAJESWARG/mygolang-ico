c3aefc23692a8eb0f08deed8e1a6015a7b0b1c8c
Brajeswar Ghosh <brajeswar.ghosh@mihup.com>
2026-03-25 17:49:19 +0530

    added webreq

--- 21webreq/main.go
+++ 21webreq/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	fmt.Println("Welcome to web verb video - LCO")
 
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
 func PerformGetRequest() {
@@ -35,4 +36,31 @@ func PerformGetRequest() {
 
 	fmt.Println("ByteCount is: ", byteCount)
 	fmt.Println(responseString.String())
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"coursename": "Let's go with golang",
+			"price": 0,
+			"platform":"LearnCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+
 }
