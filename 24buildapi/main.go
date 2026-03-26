a7176290b5192ab54a01ed4d37d0df0414b36105
Brajeswar Ghosh <brajeswar.ghosh@mihup.com>
2026-03-26 17:12:55 +0530

    added buildapi

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,1 +1,28 @@
-package buildapi
+package main
+
+import "fmt"
+
+// Model for course -file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+var course []Course
+
+// middleware, helper -file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+	fmt.Println("Welcome")
+}
